c2abfa48e3cf9017017211dfe45c9f907959fdbb
luschmar <90399580+luschmar@users.noreply.github.com>
2021-12-07 10:01:56 +0100

    solve day07

--- day-07/src/test/java/Day07Test.java
+++ day-07/src/test/java/Day07Test.java
@@ -1,34 +1,54 @@
 import org.junit.jupiter.params.ParameterizedTest;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day07Test {
 	@ParameterizedTest
 	@AocFileSource(inputs = {
-			@AocInputMapping(input="test.txt", solution="fail"),
-			@AocInputMapping(input="input.txt", solution="fail")
+			@AocInputMapping(input="test.txt", solution="37"),
+			@AocInputMapping(input="input.txt", solution="323647")
 	}
 	)
 	void part1(Stream<String> input, String solution) {
 		var inputList = input.collect(Collectors.toList());
-
-		long res= 0;
+		var values = Arrays.asList(inputList.get(0).split(","));
+		var sum = new int[values.size()];
+		for(int i = 0; i < values.size(); i++) {
+			
+			for(int j = 0; j < sum.length; j++) {
+				sum[j] += Math.abs(Integer.parseInt(values.get(i)) - j);
+			}
+		}
+				
+		long res = Arrays.stream(sum).min().getAsInt();
 		assertEquals(solution, Long.toString(res));
 	}
 
 	@ParameterizedTest
 	@AocFileSource(inputs = {
-			@AocInputMapping(input="test.txt", solution="fail"),
-			@AocInputMapping(input="input.txt", solution="fail")
+			@AocInputMapping(input="test.txt", solution="168"),
+			@AocInputMapping(input="input.txt", solution="87640209")
 	}
 	)
 	void part2(Stream<String> input, String solution) {
 		var inputList = input.collect(Collectors.toList());
-
-		long res= 0;
+		var values = Arrays.asList(inputList.get(0).split(","));
+		var sum = new int[values.size()];
+		for(int i = 0; i < values.size(); i++) {
+			int bla = Integer.parseInt(values.get(i));
+			for(int j = 0; j < sum.length; j++) {
+				sum[j] += IntStream.range(1, Math.abs(bla - j )+1).sum();
+			}
+		}
+		System.out.println(Arrays.stream(sum).mapToObj(Integer::toString).collect(Collectors.joining(",")));
+				
+		long res = Arrays.stream(sum).min().getAsInt();
 		assertEquals(solution, Long.toString(res));
 	}
 }
